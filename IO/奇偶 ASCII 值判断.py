968e98fd5723a109470d9074b87a4783f850e8b7
Yue Wang <1743520574@qq.com>
2026-04-16 22:26:21 +0800

    day 20

---
+++ IO/奇偶 ASCII 值判断.py
@@ -0,0 +1,8 @@
+s = input()
+
+number = ord(s)
+
+if number % 2 == 0:
+    print("N0")
+else:
+    print("Yes")
---
+++ IO/晶晶赴约会.py
@@ -0,0 +1,8 @@
+day = int(input())
+
+lst = [i for i in range(1, 7, 2)]
+
+if day in lst:
+    print("NO")
+else:
+    print("YES")
